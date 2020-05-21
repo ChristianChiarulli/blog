8a7d4786fc97ce10e2e86b296dd7e790108b7471
Chris <chrisatmachine@gmail.com>
2020-05-21 13:12:08 -0400

    some updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,101 +18,96 @@ import {
 const IndexPage = () => {
   return (
     <Layout>
-      <div style={{ paddingTop: "30px" }}>
-        <SEO title="Home" keywords={["chris@machine"]} />
-        <div className={style.card}>
-          <div className={style.post}>
-            <div className={style.cover}>
-              <img
-                src={Me}
-                height="200px"
-                width="200px"
-                style={{ borderRadius: "50%" }}
-                alt="Me"
-              />
-            </div>
-            <div className={style.content}>
-              <span>
-                My name is Christian, you're probably here for my blog.
-              </span>
-              <div className={style.list}>
-                <ul>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      href={"https://github.com/ChristianChiarulli"}
-                    >
-                      <FaGithub color="#FFFFFF" size="30" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      href={
-                        "https://www.youtube.com/channel/UCS97tchJDq17Qms3cux8wcA"
-                      }
-                    >
-                      <FaYoutube color="#EB3323" size="30" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      href={"https://discord.gg/Xb9B4Ny"}
-                    >
-                      <FaDiscord color="#768AD4" size="30" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      href={"https://www.linkedin.com/in/christianchiarulli/"}
-                    >
-                      <FaLinkedin color="#3077B0" size="30" />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      href={"https://www.patreon.com/chrisatmachine"}
-                    >
-                      <FaPatreon color="#E8715C" size="30" />
-                    </a>
-                  </li>
-                </ul>
-              </div>
+      <h1 style={{ textAlign: "center", marginTop: "80px" }}>Portfolio</h1>
+      <SEO title="Home" keywords={["chris@machine"]} />
+      <div className={style.card}>
+        <div className={style.post}>
+          <div className={style.cover}>
+            <img
+              src={Me}
+              height="200px"
+              width="200px"
+              style={{ borderRadius: "50%" }}
+              alt="Me"
+            />
+          </div>
+          <div className={style.content}>
+            <span>My name is Christian, you're probably here for my blog.</span>
+            <div className={style.list}>
+              <ul>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="nofollow noopener noreferrer"
+                    href={"https://github.com/ChristianChiarulli"}
+                  >
+                    <FaGithub color="#FFFFFF" size="30" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="nofollow noopener noreferrer"
+                    href={
+                      "https://www.youtube.com/channel/UCS97tchJDq17Qms3cux8wcA"
+                    }
+                  >
+                    <FaYoutube color="#EB3323" size="30" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="nofollow noopener noreferrer"
+                    href={"https://discord.gg/Xb9B4Ny"}
+                  >
+                    <FaDiscord color="#768AD4" size="30" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="nofollow noopener noreferrer"
+                    href={"https://www.linkedin.com/in/christianchiarulli/"}
+                  >
+                    <FaLinkedin color="#3077B0" size="30" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="nofollow noopener noreferrer"
+                    href={"https://www.patreon.com/chrisatmachine"}
+                  >
+                    <FaPatreon color="#E8715C" size="30" />
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className={style.card}>
-          <div className={style.post}>
-            <div className={style.cover}>
-              <img src={Nvim} height="200px" width="200px" alt="nvim" />
-            </div>
-            <div className={style.content}>
-              <span>
-                I have a pretty popular Neovim config over on Github if you're
-                interested. A lot of the process to create it has been
-                documented <Link to={"/neovim"}>here</Link>.
-              </span>
-            </div>
+      </div>
+      <div className={style.card}>
+        <div className={style.post}>
+          <div className={style.cover}>
+            <img src={Nvim} height="200px" width="200px" alt="nvim" />
+          </div>
+          <div className={style.content}>
+            <span>
+              I have a pretty popular Neovim config over on Github if you're
+              interested. A lot of the process to create it has been documented{" "}
+              <Link to={"/neovim"}>here</Link>.
+            </span>
           </div>
         </div>
-        <div className={style.card}>
-          <div className={style.post}>
-            <div className={style.cover}>
-              <img src={ThisBlog} height="150px" width="225px" alt="nvim" />
-            </div>
-            <div className={style.content}>
-              <span>
-                Is available on my github you can fork it if you want.
-              </span>
-            </div>
+      </div>
+      <div className={style.card}>
+        <div className={style.post}>
+          <div className={style.cover}>
+            <img src={ThisBlog} height="150px" width="225px" alt="nvim" />
+          </div>
+          <div className={style.content}>
+            <span>Is available on my github you can fork it if you want.</span>
           </div>
         </div>
       </div>
