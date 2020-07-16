3f74c3623d8feb95e86bb6eb3301fd7da2b5341a
Chris <chrisatmachine@gmail.com>
2020-07-16 00:43:16 -0400

    fixing form submission key

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ function Contact() {
       <SEO title="Contact" keywords={["chris@machine"]} />
       <h1 style={{ textAlign: "center" }}>Contact</h1>
       <div className={style.container}>
-        <ContactForm action="https://www.flexyform.com/f/c5dd67b083eb48f8b3e069cff1f21d474f553deb"></ContactForm>
+        <ContactForm action="https://www.flexyform.com/f/9035a58cef335a9fec860fc1af4bb9b01d4f3f4f	"></ContactForm>
       </div>
     </Layout>
   )
